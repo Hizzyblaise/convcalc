657ec884103b3b38e935e2d81c6535e421d3da4b
Isaac Adebayo <67612661+Hizzyblaise@users.noreply.github.com>
2020-06-30 00:56:30 +0100

    Add files via upload
    pip install streamlit 
    
    streamlit run conversioncalculator.py

---
+++ conversioncalculator.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+# Function to convert Dana to kilogrammes
+def dantokilo(value):
+    result = value * 5.0
+    return result
+
+# Function to convert Garawa to Kilogrammes
+def gartokilo(value):
+    result = value * 40.0
+    return result
+
+st.subheader('CONVERSION CALCULATOR')
+def mainmenu():
+    choice = st.selectbox(
+        "What conversion do you like to perform? ",
+        [
+            "1.Convert from Dana to Kilogrammes",
+            "2.Convert from Garawa to Kilogrammes"
+        ]
+    )
+    return choice
+
+
+choice = mainmenu()
+
+ivalue = float(st.text_input("Enter the value you want to convert: "))
+
+if choice == "1.Convert from Dana to Kilogrammes":
+    st.write(str(ivalue) + " dana to Kg is " + str(dantokilo(ivalue)) + "kg")
+elif choice == "2.Convert from Garawa to Kilogrammes":
+    st.write(str(ivalue) + " garawa to Kg is " + str(gartokilo(ivalue)) + "kg")
+
+
+
